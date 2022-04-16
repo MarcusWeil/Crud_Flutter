54bd5427afa75dbb43639f706ac5af04a96e3367
MarcusWeil <vinicciusplay10@gmail.com>
2022-04-16 18:37:28 -0400

    ProductModel
    Criando o model para implementação futura de Product.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,2 @@
+// ignore: constant_identifier_names
+const String URL_BASE = "http://localhost:3001";
---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  String id;
+  final String name;
+  final String price;
+  final String dateCreated;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.dateCreated,
+  });
+}
