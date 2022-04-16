50d0225be160b9c882c86fd521c4809882b4e887
MarcusWeil <vinicciusplay10@gmail.com>
2022-04-15 23:05:09 -0400

    Create Product Page
    Configurando a página de Adição de produtos, com botões elevados e TextFields em um Stack.

--- lib/view/product_create.dart
+++ lib/view/product_create.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CreateProduct extends StatefulWidget {
   const CreateProduct({Key? key}) : super(key: key);
-  //A classe CreateProduct terá um estado de página, esse estado será criado e salvo.
+  //A classe CreateProduct terá um estado de página, esse estado será atualizado conforme houverem os eventos e será salvo.
   @override
   CreateProductPageState createState() => CreateProductPageState();
 }
@@ -21,7 +21,7 @@ class CreateProductPageState extends State<CreateProduct> {
 
 _appBar() {
   return AppBar(
-      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      backgroundColor: const Color.fromARGB(255, 12, 116, 202),
       title: Align(
           alignment: Alignment.center,
           child: Row(
@@ -36,4 +36,93 @@ _appBar() {
           )));
 }
 
-_body() {}
+_body() {
+  return SingleChildScrollView(
+    child: Container(
+      padding: const EdgeInsets.all(12),
+      child: Stack(children: [
+        Column(
+          children: [
+            const Text("Everything is fine!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              children: const [
+                Expanded(
+                  child: Text(
+                      "There are no shipments with suspicious activity.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20.0)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Image.asset("assets/create-product.png",
+                width: 200.0, height: 200.0),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const TextField(
+              onChanged: (null),
+              decoration: InputDecoration(
+                  label: Text('Nome do Produto'),
+                  suffixIcon: Icon(
+                    Icons.dashboard,
+                    color: Colors.black,
+                  )),
+            ),
+            const SizedBox(height: 15),
+            const TextField(
+              onChanged: (null),
+              decoration: InputDecoration(
+                  label: Text('Preço do Produto'),
+                  suffixIcon: Icon(
+                    Icons.attach_money,
+                    color: Colors.black,
+                  )),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color.fromARGB(255, 12, 116, 202),
+                        elevation: 5),
+                    child: const Text(
+                      'Adicionar',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 255, 255, 255),
+                      elevation: 5),
+                  child: const Text(
+                    'Cancelar',
+                    style: TextStyle(
+                        fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                )
+              ],
+            ),
+          ],
+        )
+      ]),
+    ),
+  );
+}
