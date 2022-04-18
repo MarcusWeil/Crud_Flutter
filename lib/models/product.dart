20243c899b153a02bde32c15fec5e83811eaa9ff
MarcusWeil <vinicciusplay10@gmail.com>
2022-04-17 21:39:07 -0400

    Product Model JsonGetter
    Serviço http para coletar as informações do JSON no backend da aplicação CRUD em Angular

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,5 @@
 class Product {
-  String id;
+  final String id;
   final String name;
   final String price;
   final String dateCreated;
@@ -10,4 +10,13 @@ class Product {
     required this.price,
     required this.dateCreated,
   });
+
+//Os valores serão obtidos dinâmicamente pelo fromJson, que retornará a classe com seus devidos valores.
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+        id: json['id'],
+        name: json['name'],
+        price: json['price'],
+        dateCreated: json['dateCreated']);
+  }
 }
